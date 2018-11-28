702d44369e1658ef4c3cd6943dfafe8f56bba866
Yoshinari Fujinuma <fujinumay@gmail.com>
2018-11-28 09:27:37 -0700

    updates

---
+++ src/ml_debugger_cli/menu_corpus.py
@@ -0,0 +1,6 @@
+from menu import Menu
+
+class MenuCorpus(Menu):
+    def __init__(self):
+        super(Menu, self).__init__()  # Calling the parent class constructor
+        self._actions = ["Show sample sentences", "Read Corpus", "Exit"]
